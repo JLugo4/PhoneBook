3572caa220613f240af9867b831554f4887ca9dd
Jarek <jarek@Zipcoders-MacBook-Pro-65.local>
2023-03-14 18:38:01 -0400

    Working on reverseLooup

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,11 +1,8 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 //import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
+
 
 /**
  * Created by leon on 1/23/18.
@@ -24,24 +21,37 @@ public class PhoneBook {
     }
 
     public void add(String name, String phoneNumber) {
+        phonebook.put(name, Collections.singletonList(phoneNumber));
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        List<String> newNumbers = new ArrayList<>();
+        newNumbers.addAll(List.of(phoneNumbers));
+        phonebook.put(name, newNumbers);
     }
 
     public void remove(String name) {
+        phonebook.remove(name);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+        if (Objects.requireNonNull(phonebook).containsKey(name))
+            return true;
+        return false;
     }
 
+
     public List<String> lookup(String name) {
+        if (phonebook.containsKey(name)) {
+            return Collections.singletonList(name);
+        }
         return null;
     }
 
     public String reverseLookup(String phoneNumber)  {
-        return null;
+        for(String n : phonebook.keySet());
+        if( phonebook.get(n)
+
     }
 
     public List<String> getAllContactNames() {
